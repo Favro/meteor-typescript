2b212901e97406ff50ff4653c412d87cce5f4590
Oleksandr Chekhovskyi <oleksandr.chekhovskyi@hansoft.com>
2015-04-28 09:45:26 +0200

    Added more type definitions.

--- definitions/ironrouter.d.ts
+++ definitions/ironrouter.d.ts
@@ -11,8 +11,21 @@ declare module Router {
 		route: Route;
 	}
 
+	interface RouteConfig {
+		name?: string;
+		path?: string;
+		where?: string;
+		action?: () => void;
+		onBeforeAction?: () => void;
+		onStop?: () => void;
+		template?: string;
+	}
+
+	type RouteCallback = () => void;
+
 	function configure(config: Config): void;
-	function route(path: string, func: () => void): void;
-	function go(path: string): void;
+	function route(path: string, config: RouteConfig | RouteCallback): void;
+	function go(nameOrPath: string, params?: any, options?: any): void;
 	function current(): Controller;
+	function onBeforeAction(func: () => void): void;
 }
--- definitions/meteor.d.ts
+++ definitions/meteor.d.ts
@@ -1,6 +1,7 @@
 declare module Mongo {
 	class ObjectID {
 		constructor(hexString?: string);
+		toHexString(): string;
 	}
 
 	interface SortSpecifier {
@@ -17,10 +18,14 @@ declare module Mongo {
 		fetch(): T[];
 	}
 
-	class Collection<T> {
-		constructor(name: string);
+	var Collection: CollectionStatic;
 
-		find(selector: any, options?: {
+	interface CollectionStatic {
+		new<T>(name: string): Collection<T>;
+	}
+
+	interface Collection<T> {
+		find(selector?: any, options?: {
 			sort?: SortSpecifier;
 			skip?: number;
 			limit?: number;
@@ -29,7 +34,7 @@ declare module Mongo {
 			transform?: (doc: T) => T;
 		}): Cursor<T>;
 
-		findOne(selector: any, options?: {
+		findOne(selector?: any, options?: {
 			sort?: SortSpecifier;
 			skip?: number;
 			fields?: FieldSpecifier;
@@ -75,26 +80,59 @@ declare module Meteor {
 		isDefaultPrevented(): boolean;
 	}
 
-	interface Template {
-		helpers(helpers: {
-			[name: string]: (...args: any[]) => Object;
-		}): void;
+	interface HelpersMap {
+		[name: string]: any;
+	}
 
-		events(eventMap: {
-			[selector: string]: (event: Event) => void;
-		}): void;
+	interface EventsMap {
+		[selector: string]: (event?: Event, tpl?: TemplateInstance) => void;
+	}
+
+	interface Template {
+		helpers(helpers: HelpersMap): void;
+		events(eventMap: EventsMap): void;
 
 		onRendered(callback: () => void): void;
 		onCreated(callback: () => void): void;
 		onDestroyed(callback: () => void): void;
+
+		rendered: () => void;
+		created: () => void;
+		destroyed: () => void;
 	}
 
 	interface TemplateInstance {
-		// FIXME.
+		$(selector: string): JQuery;
+		find(selector: string): HTMLElement;
+		findAll(selector: string): HTMLElement[];
+		firstNode: HTMLElement;
+		lastNode: HTMLElement;
+		data: any;
+	}
+
+	interface UserEmail {
+		address: string;
+		verified: boolean;
+	}
+
+	interface UserProfile {
+		name: string;
+	}
+
+	interface UserServices {
+		google: {
+			email: string;
+			verified_email: boolean;
+			picture: string;
+		};
 	}
 
 	interface User {
-		_id: string;
+		_id?: string;
+		createdAt?: Date;
+		emails?: UserEmail[];
+		profile?: UserProfile;
+		services?: UserServices;
 	}
 
 	interface SubscriptionHandle {
@@ -109,20 +147,180 @@ declare module Meteor {
 
 	function user(): User;
 	function userId(): string;
+	function loggingIn(): boolean;
+	function logout(callback?: (err?: any) => void): void;
+
+	function startup(func: () => void): void;
+	function wrapAsync(func: Function, context?: any): Function;
+
+	function setTimeout(func: () => void, delay: number): number;
+	function setInterval(func: () => void, delay: number): number;
+	function clearTimeout(id: number): void;
+	function clearInterval(id: number): void;
+	function defer(func: () => void): void;
+
+	function absoluteUrl(path?: string, options?: {
+		secure?: boolean;
+		replaceLocalhost?: boolean;
+		rootUrl?: string;
+	}): string;
+
+	function _debug(...args: any[]): void;
+
+	interface LoginOptions {
+		requestPermissions?: string[];
+		requestOfflineToken?: boolean;
+		forceApprovalPrompt?: boolean;
+		userEmail?: string;
+		loginStyle?: string;
+	}
+
+	function loginWithGoogle(options?: LoginOptions, callback?: (err?: any) => void): void;
+	function loginWithPassword(user: Object | string, password: string, callback?: (err?: any) => void): void;
+
+	function status(): {
+		connected: boolean;
+		status: string;
+		retryCount: number;
+		retryTime?: number;
+		reason?: string;
+	};
 
 	var users: Mongo.Collection<User>;
+
+	var isClient: boolean;
+	var isServer: boolean;
+	var isCordova: boolean;
+
+	var settings: any;
+	var release: string;
+}
+
+declare module EJSON {
+	function parse(str: string): any;
+	function stringify(val: any, options: {
+		indent?: boolean | number | string;
+		canonical?: boolean;
+	}): string;
+	function fromJSONValue(val: any): any;
+	function toJSONValue(val: any): any;
+	function equals(a: any, b: any, options: {
+		keyOrderSensitive?: boolean;
+	}): boolean;
+	function clone(val: any): any;
+	function newBinary(): any;
+	function isBinary(x: any): boolean;
+	function addType(name: string, factory: Function): void;
+}
+
+declare module Assets {
+	function getText(assetPath: string): string;
+}
+
+declare module Session {
+	function set(key: string, value: any): void;
+	function setDefault(key: string, value: any): void;
+	function get(key: string): any;
+	function equals(key: string, value: any): boolean;
 }
 
 declare module Accounts {
+	interface EmailCallbacks {
+		from?: () => string;
+		subject?: (user: Meteor.User) => string;
+		text?: (user: Meteor.User, url: string) => string;
+		html?: (user: Meteor.User, url: string) => string;
+	}
+
+	interface EmailTemplates {
+		from?: string;
+		siteName?: string;
+		headers?: any;
+		resetPassword?: EmailCallbacks;
+		enrollAccount?: EmailCallbacks;
+		verifyEmail?: EmailCallbacks;
+	}
+
+	interface URLs {
+		verifyEmail?: (token: string) => string;
+		resetPassword?: (token: string) => string;
+	}
+
+	type EmailDomainCallback = (email: string) => boolean;
+
 	interface LoginAttempt {
+		type: string;
+		allowed: boolean;
+		error: any;
 		user: Meteor.User;
+		connection: any;
+		methodName: string;
+		methodArguments: any[];
+	}
+
+	interface CreateUserOptions {
+		username?: string;
+		email?: string;
+		password: string;
+		profile?: Meteor.UserProfile;
 	}
 
 	function validateLoginAttempt(func: (attempt: LoginAttempt) => any): void;
+	function onLogin(func: (attempt: LoginAttempt) => void): void;
+	function onCreateUser(func: (options: CreateUserOptions, user: Meteor.User) => Meteor.User): void;
+	function onEmailVerificationLink(func: (token: string, done: Function) => void): void;
+	function verifyEmail(token: string, callback?: (err?: any) => void): void;
+
+	function forgotPassword(options: {
+		email?: string;
+	}, callback?: (err?: any) => void): void;
+
+	function resetPassword(token: string, newPassword: string, callback?: (err?: any) => void): void;
+
+	function config(options: {
+		sendVerificationEmail?: boolean;
+		forbidClientAccountCreation?: boolean;
+		restrictCreationByEmailDomain?: string | EmailDomainCallback;
+		loginExpirationInDays?: number;
+		oauthSecretKey?: string;
+	}): void;
+
+	function createUser(options: CreateUserOptions, callback?: (err?: any) => void): void;
+
+	function loginServicesConfigured(): boolean;
+
+	var emailTemplates: EmailTemplates;
+	var urls: URLs;
+}
+
+declare module Email {
+	interface Options {
+		from: string;
+		to?: string | string[];
+		cc?: string | string[];
+		bcc?: string | string[];
+		replyTo?: string | string[];
+		subject?: string;
+		text?: string;
+		html?: string;
+		headers?: any;
+	}
+
+	function send(options: Options): void;
+}
+
+declare module DDP {
+	interface RandomStream {
+		hexString(numChars: number): string;
+	}
+
+	function randomStream(): RandomStream;
 }
 
 declare module Tracker {
 	function autorun(func: () => void): void;
+	function flush(): void;
+	function afterFlush(func: () => void): void;
 }
 
 declare class ReactiveVar<T> {
@@ -133,8 +331,53 @@ declare class ReactiveVar<T> {
 
 declare module Template {
 	function instance(): Meteor.TemplateInstance;
+	function currentData(): any;
+	function parentData(numLevels?: number): any;
+	function registerHelper(name: string, func: Function): void;
 
 	var body: Meteor.Template;
 }
 
+declare module Match {
+	var Any: any;
+	var Integer: any;
+	function ObjectIncluding(fields: any): any;
+	function Optional(pattern: any): any;
+	function OneOf(...patterns: any[]): any;
+	function Where(condition: Function): any;
+}
+
 declare function check(value: any, pattern: any): void;
+
+declare module Npm {
+	function require(name: string): any;
+}
+
+declare module ServiceConfiguration {
+	var configurations: Mongo.Collection<any>;
+}
+
+declare module Blaze {
+	interface Template {}
+
+	interface View {
+		templateInstance(): Meteor.TemplateInstance;
+	}
+
+	function render(templateOrView: Template | View, parentNote: HTMLElement, nextNode?: HTMLElement, parentView?: View): void;
+	function renderWithData(templateOrView: Template | View, data: any, parentNote: HTMLElement, nextNode?: HTMLElement, parentView?: View): void;
+	function getData(elementOrView?: HTMLElement | View): any;
+	function getView(element?: HTMLElement): View;
+	function remove(renderedView: View): void;
+
+	var currentView: View;
+}
+
+declare module Random {
+	function id(n?: number): string;
+	function secret(n?: number): string;
+	function fraction(): number;
+	function choice<T>(arrayOrString: Array<T>): T;
+	function choice(arrayOrString: string): string;
+	function hexString(n: number): string;
+}
